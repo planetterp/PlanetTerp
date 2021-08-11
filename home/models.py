b6d1870b9f65f2a03fb9324f9a043fc8f6db7bde
NoahSandler <Noahsandler@gmail.com>
2021-08-10 21:44:07 -0400

    Remove comment

--- home/models.py
+++ home/models.py
@@ -167,7 +167,6 @@ class Section(Model):
     def __str__(self):
         return f"{self.course} ({self.section_number}) taught by {self.professors}"
 
-# TODO replace login/permission decorators with appropriate mixins
 class User(AbstractUser):
     send_review_email = BooleanField(default=True)
     email = EmailField(
