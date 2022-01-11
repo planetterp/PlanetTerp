db8c5118375769a5f4d792097b96a9c726448fca
Liam DeVoe <orionldevoe@gmail.com>
2022-01-10 22:26:58 -0500

    hopefully fix ourumd migration issues

--- webpy_to_django.py
+++ webpy_to_django.py
@@ -126,14 +126,19 @@ def migrate_reviews(users, courses, professors):
         if row["reviewer_id"] == -1:
             # from ourumd, create new user
             username = row["reviewer_name"]
+
+            # ...unless a user by that name already exists
+            if username in ourumd_users:
+                continue
+
             user = User.objects.create_ourumd_user(username)
-            ourumd_users[row["reviewer_id"]] = user
+            ourumd_users[username] = user
 
     def _user(row):
         if row["reviewer_id"] == 0:
             return None
-        if row["reviewer_id"] in ourumd_users:
-            return ourumd_users[row["reviewer_id"]]
+        if row["username"] in ourumd_users:
+            return ourumd_users[row["username"]]
         return _foreign_key(users, row, "reviewer_id")
 
     def _status(row):
