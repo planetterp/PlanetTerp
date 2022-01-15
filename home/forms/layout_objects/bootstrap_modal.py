0318feb52fd56e57728dcbeca797071c7dc4788a
Noah Sandler <noahsandler@gmail.com>
2022-01-14 21:32:24 -0500

    Update bootstrap modal layout object

--- home/forms/layout_objects/bootstrap_modal.py
+++ home/forms/layout_objects/bootstrap_modal.py
@@ -5,44 +5,54 @@ from crispy_forms.utils import flatatt, TEMPLATE_PACK
 
 
 class BootstrapModal(LayoutObject):
-    '''
-    Custom crispy_forms layout object that displays forms fields
-    inside a bootstrap4 modal. This layout object works like all other
-    crispy_forms layout objects:
-    - Initialize the object with the desired form fields
-    - Add any html attribute to the modal via kwargs using the following keys:
-        - css_id: The modal id, or the id of the outermost <div>
-        - css_class: The modal class name(s), or the class(es) of the outermost <div>
-            - The "modal" and "fade" classes are applied by default
-        - title_id: The id attribute of the title tag
-        - title: The text to display in the modal header. The text will be wrapped in
-                 a <h5> tag
-    '''
-    css_class = "modal fade"
+    """
+    Boostrap layout object for rendering crispy forms objects inside a bootstrap modal.
+
+    The following attributes can be set:
+        - `css_id`: modal's DOM id
+        - `css_class`: modal's DOM classes
+            - NOTE: "modal" and "fade" are applied by default
+        - `title`: text to display in the modal's header
+            - NOTE: text will be wrapped in a <h5> tag
+        - `title_id`: title's DOM id
+        - `title_class`: titles's DOM classes
+            - NOTE: "modal-title" is applied by default
+
+    Example::
+
+        Modal(
+            'field1',
+            Div('field2'),
+            css_id="modal-id-ex",
+            css_class="modal-class-ex,
+            title="This is my modal"
+        )
+    """
+
+    template = "bootstrap_modal.html"
 
     def __init__(self, *fields, **kwargs):
         self.fields = list(fields)
-        self.template = "bootstrap_modal.html"
+        self.template = kwargs.pop("template", self.template)
 
-        if hasattr(self, "css_class") and "css_class" in kwargs:
-            self.css_class += " %s" % kwargs.pop("css_class")
-        if not hasattr(self, "css_class"):
-            self.css_class = kwargs.pop("css_class", None)
-
-        self.css_id = kwargs.pop("css_id", "")
-        self.title_id = kwargs.pop("title_id", "modal_title_id")
+        self.css_id = kwargs.pop("css_id", "modal_id")
         self.title = kwargs.pop("title", "Modal Title")
+        self.title_id = kwargs.pop("title_id", "modal_title_id")
 
-        kwargs = {
-            **kwargs,
-            "tabindex": "-1",
-            "role": "dialog",
-            "aria-labelledby": "%s-label" % self.css_id
-        }
+        self.css_class = "modal fade"
+        if "css_class" in kwargs:
+            self.css_class += " %s" % kwargs.pop("css_class")
+
+        self.title_class = "modal-title"
+        if "title_class" in kwargs:
+            self.title_class += " %s" % kwargs.pop("title_class")
+
+        kwargs = {**kwargs, "tabindex": "-1", "role": "dialog", "aria-labelledby": "%s-label" % self.css_id}
 
         self.flat_attrs = flatatt(kwargs)
 
     def render(self, form, form_style, context, template_pack=TEMPLATE_PACK, **kwargs):
         fields = self.get_rendered_fields(form, form_style, context, template_pack, **kwargs)
+        template = self.get_template_name(template_pack)
 
-        return render_to_string(self.template, {"modal": self, "fields": fields})
+        return render_to_string(template, {"modal": self, "fields": fields})
