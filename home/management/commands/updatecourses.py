9a8d5a504e69b672ac66c27463a4339222ec7020
nsandler1 <Noahsandler@gmail.com>
2022-08-30 15:08:49 -0400

    refractor code

--- home/management/commands/updatecourses.py
+++ home/management/commands/updatecourses.py
@@ -1,5 +1,4 @@
 import requests
-import json
 
 from django.core.management import BaseCommand, CommandError
 
@@ -16,45 +15,46 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for semester in options['semesters']:
-            response = requests.get("https://api.umd.io/v1/courses", params={"semester": semester})
-            course_data = json.loads(response.content)
-            for item in course_data:
-                course = Course.unfiltered.filter(name=item['course_id']).first()
-                if not course:
-                    course = Course(
-                        name=item['course_id'],
-                        department=item['dept_id'],
-                        course_number=item['course_id'][-3:],
-                        title=item['name'],
-                        credits=item['credits'],
-                        description=self._description(item["relationships"])
-                    )
+            kwargs = {"semester": semester, "per_page": 100, "page": 1}
+            course_data = requests.get("https://api.umd.io/v1/courses", params=kwargs).json()
+            while course_data:
+                for item in course_data:
+                    course = Course.unfiltered.filter(name=item['course_id']).first()
+                    if not course:
+                        course = Course(
+                            name=item['course_id'],
+                            department=item['dept_id'],
+                            course_number=item['course_id'][-3:],
+                            title=item['name'],
+                            credits=item['credits'],
+                            description=self._description(item["relationships"])
+                        )
 
-                    course.save()
-                    self.num_new_courses += 1
-                    print(f"Created course {course.name}")
+                        course.save()
+                        self.num_new_courses += 1
+                        print(f"Created course {course.name}")
 
-                professors = self._professors(course)
-                course.professors.add(*professors)
+                    professors = self._professors(course)
+                    course.professors.add(*professors)
 
+                    kwargs["page"] += 1
+                    course_data = requests.get("https://api.umd.io/v1/courses", params=kwargs).json()
 
         print(f"New Courses Added: {self.num_new_courses}")
         print(f"New Professors Added: {self.num_new_professors}")
 
     def _professors(self, course):
         ret = []
-        response = requests.get("https://api.umd.io/v1/professors", params={"course_id":course.name})
-        professor_data = json.loads(response.content)
+        kwargs = {"course_id":course.name}
+        professor_data = requests.get("https://api.umd.io/v1/professors", params=kwargs).json()
 
         for item in professor_data:
-            print(item)
             professor = Professor.unfiltered.filter(name=item['name']).first()
             if not professor:
                 professor = Professor(name=item['name'], type=Professor.Type.PROFESSOR)
                 professor.save()
                 self.num_new_professors += 1
                 print(f"  Created professor {professor.name}")
-
 
             ret.append(professor)
 
