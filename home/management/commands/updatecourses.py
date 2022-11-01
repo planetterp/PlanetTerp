061a31dc2d889e9ecbec1ac451e4b2e3701e54e8
nsandler1 <Noahsandler@gmail.com>
2022-11-01 01:52:06 -0400

    refractor code

--- home/management/commands/updatecourses.py
+++ home/management/commands/updatecourses.py
@@ -9,75 +9,79 @@ from home.utils import Semester
 class Command(BaseCommand):
     def __init__(self):
         super().__init__()
-        self.num_new_courses = 0
-        self.num_new_professors = 0
+        self.total_num_new_courses = 0
+        self.total_num_new_professors = 0
 
     def add_arguments(self, parser):
         parser.add_argument("semesters", nargs='+')
 
     def handle(self, *args, **options):
+        inputted_semesters = [Semester(s).name() for s in options['semesters']]
+        print(f"Inputted Semesters: {', '.join(inputted_semesters)}")
+
         for semester in options['semesters']:
             s = Semester(semester)
             if Semester.current() < s:
                 print(f"{s.name()} hasn't happened yet! Skipping...")
                 continue
 
-            print(f"Working on data for {s.name()}...")
+            print(f"Working on courses for {s.name()}...")
             kwargs = {"semester": semester, "per_page": 100, "page": 1}
             course_data = requests.get("https://api.umd.io/v1/courses", params=kwargs).json()
             while course_data:
-                for item in course_data:
-                    course = Course.unfiltered.filter(name=item['course_id']).first()
+                for umdio_course in course_data:
+                    course = Course.unfiltered.filter(name=umdio_course['course_id']).first()
+
                     if not course:
                         course = Course(
-                            name=item['course_id'],
-                            department=item['dept_id'],
-                            course_number=item['course_id'][4:],
-                            title=item['name'],
-                            credits=item['credits'],
-                            additional_info=item['relationships']['additional_info'],
-                            cross_listed_with=item['relationships']['also_offered_as'],
-                            credit_granted_for=item['relationships']['credit_granted_for'],
-                            formerly=item['relationships']['formerly'],
-                            prereqs=item['relationships']['prereqs'],
-                            recs=item['relationships']['recs'], # TODO: check name after umdio gets updated
-                            coreqs=item['relationships']['coreqs'],
-                            restrictions=item['relationships']['restrictions'],
+                            name=umdio_course['course_id'],
+                            department=umdio_course['dept_id'],
+                            course_number=umdio_course['course_id'][4:],
+                            title=umdio_course['name'],
+                            credits=umdio_course['credits'],
+                            additional_info=umdio_course['relationships']['additional_info'],
+                            cross_listed_with=umdio_course['relationships']['also_offered_as'],
+                            credit_granted_for=umdio_course['relationships']['credit_granted_for'],
+                            formerly=umdio_course['relationships']['formerly'],
+                            prereqs=umdio_course['relationships']['prereqs'],
+                            #recs=umdio_course['relationships']['recs'], # TODO: check name after umdio gets updated
+                            coreqs=umdio_course['relationships']['coreqs'],
+                            restrictions=umdio_course['relationships']['restrictions'],
                             # TODO: Add new field when umdio gets updated
-                            description=item["description"]
+                            description=umdio_course["description"]
                         )
 
                         course.save()
-                        self.num_new_courses += 1
+                        self.total_num_new_courses += 1
                         print(f"Created course {course.name}")
 
-                    professors = self._professors(course)
-                    course.professors.add(*professors)
+                    self._professors(course)
 
                 kwargs["page"] += 1
                 course_data = requests.get("https://api.umd.io/v1/courses", params=kwargs).json()
 
-        print(f"\n**New Courses Created: {self.num_new_courses} **")
-        print(f"**New Professors Created: {self.num_new_professors} **")
+        print(f"\n** New Courses Created: {self.total_num_new_courses} **")
+        print(f"** New Professors Created: {self.total_num_new_professors} **")
 
-    def _professors(self, course):
-        ret = []
+    def _professors(self, course: Course):
+        new_professors = set()
         kwargs = {"course_id": course.name}
-        professor_data = requests.get("https://api.umd.io/v1/professors", params=kwargs).json()
+        umdio_professors = requests.get("https://api.umd.io/v1/professors", params=kwargs).json()
+        course_professors = course.professors.all()
 
-        for item in professor_data:
+        for umdio_professor in umdio_professors:
             try:
-                professor = Professor.unfiltered.filter(name=item['name']).first()
-            except Exception:
+                professor = Professor.unfiltered.filter(name=umdio_professor['name']).first()
+            except TypeError:
                 continue
 
-            if item['name'] == "Instructor: TBA":
+            if umdio_professor['name'] == "Instructor: TBA":
                 continue
 
             if not professor:
                 # To make our lives easier, attempt to automatically verify the professor
                 # following the same criteria in admin.py
-                split_name = item['name'].strip().split()
+                split_name = umdio_professor['name'].strip().split()
                 first_name = split_name[0].lower().strip()
                 last_name = split_name[-1].lower().strip()
                 query = Professor.verified.filter(
@@ -88,7 +92,7 @@ class Command(BaseCommand):
                     Q(slug="_".join(reversed(split_name)).lower())
                 )
 
-                prof_name = " ".join([name.capitalize() for name in item['name'].split()])
+                prof_name = " ".join([name.capitalize() for name in umdio_professor['name'].split()])
                 professor = Professor(name=prof_name, type=Professor.Type.PROFESSOR)
 
                 if (not query.exists()) and len(split_name) <= 2:
@@ -96,9 +100,13 @@ class Command(BaseCommand):
                     professor.status = Professor.Status.VERIFIED
 
                 professor.save()
-                self.num_new_professors += 1
-                print(f"Created professor {professor.name} for {course.name} as {professor.status}")
+                self.total_num_new_professors += 1
+                print(f"Created professor {professor.name} for {course.name}")
 
-            ret.append(professor)
+            if professor not in course_professors:
+                new_professors.add(professor)
 
-        return ret
+        num_new_professors = len(new_professors)
+        if num_new_professors > 0:
+            print(f"Updated course {course.name} with {num_new_professors} new {'professors' if num_new_professors > 0 else 'professor'}")
+            course.professors.add(*new_professors)
