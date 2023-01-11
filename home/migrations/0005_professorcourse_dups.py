f09c2f6d76bc5f595f2197ca86e10a496144f406
nsandler1 <Noahsandler@gmail.com>
2023-01-10 21:35:34 -0500

    use safer approach

--- home/migrations/0005_professorcourse_dups.py
+++ home/migrations/0005_professorcourse_dups.py
@@ -4,21 +4,29 @@ def forwards_func(apps, schema_editor):
     ProfessorCourse = apps.get_model("home", "ProfessorCourse")
     db_alias = schema_editor.connection.alias
     professor_courses = ProfessorCourse.objects.using(db_alias).all()
-    for record in professor_courses:
-        dup_records = professor_courses.filter(
-            professor=record.professor,
-            course=record.course
-        ).exclude(pk=record.pk)
+    unique_professors = professor_courses.values_list("professor", flat=True).distinct()
+    records_to_delete = []
 
-        if dup_records.count() <= 1:
-            continue
+    for pid in unique_professors:
+        courses = professor_courses.filter(professor__pk=pid)
+        unique_courses = courses.values_list("course", flat=True).distinct()
+        for cid in unique_courses:
+            records = courses.filter(course__pk=cid).order_by("recent_semester")
 
-        for dup in dup_records:
-            if dup.recent_semester and record.recent_semester and dup.recent_semester != record.recent_semester:
-                dup_records = dup_records.exclude(pk=dup.pk)
+            if records.count() <= 1:
+                continue
 
-        dup_records.delete()
-        professor_courses = ProfessorCourse.objects.using(db_alias).all()
+            if records.exclude(recent_semester=None).exists():
+                for record in records:
+                    similar_records = records.filter(recent_semester=record.recent_semester)
+                    if not record.recent_semester:
+                        records_to_delete.append(record.pk)
+                    elif similar_records.count() > 1:
+                        records_to_delete += list(similar_records[1:].values_list("pk", flat=True))
+            else:
+                records_to_delete += list(records[1:].values_list("pk", flat=True))
+
+    professor_courses.filter(pk__in=records_to_delete).delete()
 
 class Migration(migrations.Migration):
     dependencies = [
