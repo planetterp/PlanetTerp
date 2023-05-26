aa1b2b28e4def1249f835964da6f547da7b75b92
NoahSandler <Noahsandler@gmail.com>
2023-05-26 01:27:21 -0400

    make several improvements to migration script

--- home/migrations/0012_separate_descriptions.py
+++ home/migrations/0012_separate_descriptions.py
@@ -24,18 +24,31 @@ def forwards_func(apps, schema_editor):
     # the regex group index to obtain the piece of the matched expression we're
     # interested in [2].
     regex = [
-        ("prereqs", "(Prerequisite: ([^.]+\.))|(Pre-requisite: ([^.]+\.))", 2),
-        ("coreqs", "Corequisite: ([^.]+\.)", 1),
-        ("reqs", "Recommended: ([^.]+\.)", 1),
-        ("restrictions", "(Restricted to ([^.]+\.))|(Restriction: ([^.]+\.))", 2),
-        ("cross_listed_with", "(Also offered as: ([^.]+\.))|(Cross-listed with: ([^.]+\.))", 2),
-        ("formerly", "Formerly: ([^.]+\.)", 1),
-        ("additional_info", "Additional information: ([^.]+\.)", 1),
-        ("credit_granted_for", "(Credit only granted for: ([^.]+\.))|(Credit will be granted for: ([^.]+\.))|(Credit only granted for one of the following ([^.]+\.))|(Credit will be granted for one of the following: ([^.]+\.))|(Credit granted for ([^.]+\.))", 2),
+        ("prereqs", "Pre-?requisites?:? ([^.]+\.\s?(((\s?Or)|(\s?And)) [^.]+\.)*)", 1),
+        ("coreqs", "Co-?requisites?:? ([^.]+\.)", 1),
+        ("recs", "Recommended:? ([^.]+\.)", 1),
+        ("restrictions", "(Restricted to:? ([^.]+\.))|(Restrictions?:* ([^.]+\.))", 2),
+        ("cross_listed_with", "(Also offered as:? ([^.]+\.))|(Cross-?listed with:? ([^.]+\.))", 2),
+        ("formerly", "Formerly:? ([^.]+\.)", 1),
+        ("additional_info", "Additional information:? ([^.]+\.)", 1),
+        ("additional_info", "Jointly offered w\s?it\s?h:? [^.]+\.", 0),
+        ("credit_granted_for", "(Credit only granted for:? ([^.]+\.))|(Credit will be granted for:? ([^.]+\.))|(Credit only granted for one of the following ([^.]+\.))|(Credit will be granted for one of the following:? ([^.]+\.))|(Credit granted for:? ([^.]+\.))", 2),
     ]
 
     # for each of our courses...
     for course in courses:
+        # the state of this course's columns as the description gets parsed
+        column_values = {
+            "prereqs": None,
+            "coreqs": None,
+            "recs": None,
+            "restrictions": None,
+            "cross_listed_with": None,
+            "formerly": None,
+            "additional_info": None,
+            "credit_granted_for": None
+        }
+
         # shallow copy the description so we can progressively modify it
         desc = str(course.description)
 
@@ -45,19 +58,37 @@ def forwards_func(apps, schema_editor):
         # remove edge case that appears in some of the descriptions
         desc = re.sub("\\\\n", ' ', repr(desc))
 
+        # ensure words are single-spaced
+        desc = re.sub("\s{2,}", ' ', desc)
+
         # for each tuple in the regex list...
         for column, pattern, group in regex:
             # search for the associated pattern
             match_obj = re.search(pattern, desc)
 
-            # if there's a match, set the column accordingly and remove the
-            # matched piece from the description
-            if match_obj:
-                setattr(course, column, match_obj.group(group))
-                desc = re.sub(pattern, '', desc)
+            # if there is no match, skip to the next column/pattern
+            if not match_obj:
+                continue
 
-        # Remove quote chars and any leading/trailing whitespace
-        desc = desc.strip('\'').strip('\"').strip()
+            # if there is a match, set the column accordingly and remove the
+            # matched piece from the description
+            piece = match_obj.group(group)
+            if column_values[column]:
+                column_values[column] += ' ' + piece
+            else:
+                column_values[column] = piece
+
+            desc = re.sub(pattern, '', desc)
+
+        # Remove quote chars
+        desc = desc.strip('\'\"')
+
+        # Some descriptions on testduo have links to different pages.
+        # Since we don't link to those pages, remove that part of the description.
+        desc = re.sub("(Please )?(C|c)lick here.*\.?", '', desc)
+
+        # Remove leading/trailing whitespace
+        desc = desc.strip()
 
         # If there's nothing left of desc, then there must not be any actual
         # description for this course, so set the description to None.
@@ -65,11 +96,10 @@ def forwards_func(apps, schema_editor):
         if desc in ['', 'None']:
             course.description = None
         else:
-            # Some descriptions on testduo have links to different pages.
-            # Since we don't link to those pages, remove that part of the description.
-            # Why? Because we can and it's easy to do so.
-            desc = re.sub("(Please)? (C|c)lick here (.*)(/.)?", '', desc)
             course.description = desc
+
+        for col, val in column_values.items():
+            setattr(course, col, val)
 
         course.save()
 
