5955463ff2eb0230096a74b434824ea90be86303
nsandler1 <Noahsandler@gmail.com>
2023-08-20 17:04:34 -0400

    fix grade not updating after edit

--- planetterp/static/js/professor-form.js
+++ planetterp/static/js/professor-form.js
@@ -116,10 +116,11 @@ function submitProfessorForm(form_id, form_type) {
                     }
 
                     var grade_el = $(`#grade-${review_id}`);
-                    if (data["grade"] && grade_el.next("br").length == 0) {
-                        $(`#grade-${review_id}`).html(grade(data["grade"]));
+                    if (data["grade"])
+                        grade_el.html(grade(data["grade"]));
+
+                    if (grade_el.next("br").length == 0)
                         grade_el.after("<br>");
-                    }
 
                     delete data["unverify"];
                     delete data["success"];
